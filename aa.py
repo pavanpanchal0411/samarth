f9816520bfdd377b9068a8f0ca107cc53f4e28f4
pavanpanchal0411 <pavanpanchal0411@gmail.com>
2025-06-05 20:02:43 +0530

    aa.py

--- aa.py
+++ aa.py
@@ -8,7 +8,7 @@ def login():
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     if st.button("Login"):
-        if username == "admin" and password == "1234":
+        if username == "SDF" and password == "1234":
             st.session_state["logged_in"] = True
             st.success("Login successful!")
         else:
